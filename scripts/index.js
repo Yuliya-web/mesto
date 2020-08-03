782aca5850b819d802ddb5c59dee62d1e7604653
Yuliya Kukharskaya <fuagra555@mail.ru>
2020-08-03 15:55:28 +0300

    feat: second-ready

--- scripts/index.js
+++ scripts/index.js
@@ -3,32 +3,28 @@ let profile = document.querySelector('.profile');
 let editButton = profile.querySelector('.profile__edit-button');
 let container = popup.querySelector('.popup__container');
 let closeButton = container.querySelector('.popup__close-button');
-let saveButton = container.querySelector('.popup__save-button');
-let Name = container.querySelector('#Name');
-let About = container.querySelector('#About');
+let name = container.querySelector('#Name');
+let about = container.querySelector('#About');
 let nameProfile = profile.querySelector('.profile__title');
 let aboutProfile = profile.querySelector('.profile__subtitle');
 
-Name.value = 'Жак-Ив Кусто';
-About.value = 'Исследователь океана';
-
 function openPopup(){
   popup.classList.add('popup_opened');
+  name.value = nameProfile.textContent;
+  about.value = aboutProfile.textContent;
 }
-editButton.addEventListener('click', openPopup);
 
 function closePopup(){
   popup.classList.remove('popup_opened');
 }
-closeButton.addEventListener('click', closePopup);
 
 function formSubmitHandler(evt){
   evt.preventDefault();
-  let Name = container.querySelector('#Name');
-  let About = container.querySelector('#About');
-  nameProfile.textContent = Name.value;
-  aboutProfile.textContent = About.value;
-  popup.classList.remove('popup_opened');
+  nameProfile.textContent = name.value;
+  aboutProfile.textContent = about.value;
+  closePopup();
 }
 
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 container.addEventListener('submit', formSubmitHandler);
