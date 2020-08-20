0c32f684d40f07afb34146ad0b15d1435a3d75f6
Yuliya Kukharskaya <fuagra555@mail.ru>
2020-08-20 12:10:51 +0300

    fixed-bugs

--- scripts/index.js
+++ scripts/index.js
@@ -1,157 +1,101 @@
-const popup = document.querySelector('.popup');
+const popup = document.querySelector('.popup'); //попап редактирования профиля
 const profile = document.querySelector('.profile');
-const editButton = profile.querySelector('.profile__edit-button');
-const container = popup.querySelector('.popup__container');
-const closeButton = container.querySelector('.popup__close-button');
-const name = container.querySelector('#Name');
-const about = container.querySelector('#About');
-const nameProfile = profile.querySelector('.profile__title');
-const aboutProfile = profile.querySelector('.profile__subtitle');
+const editButton = profile.querySelector('.profile__edit-button'); //кнопка редактирования профиля
+const container = popup.querySelector('.popup__container'); 
+const closeButton = container.querySelector('.popup__close-button');//кнопка закрытия попапа профиля
+const name = container.querySelector('#Name'); //имя в форме
+const about = container.querySelector('#About'); //работа в форме
+const nameProfile = profile.querySelector('.profile__title'); //имя на страничке
+const aboutProfile = profile.querySelector('.profile__subtitle'); //работа на страничке
 
-const popupAdd = document.querySelector('.add-popup');
-const addButton = profile.querySelector('.profile__add-button');
-const addContainer = popupAdd.querySelector('.add-popup__container');
-const namePic = addContainer.querySelector('#namePic');
-const linkPic = addContainer.querySelector('#link');
-const closeButtonPic = addContainer.querySelector('.add-popup__close-button');
-const elementContainer = document.querySelector('.elements');
+const popupAdd = document.querySelector('.add-popup');//попап добавления элемента
+const addButton = profile.querySelector('.profile__add-button');//кнопка создать
+const addContainer = popupAdd.querySelector('.add-popup__container');//контейнер в попапе добавления
+const closeButtonPic = addContainer.querySelector('.add-popup__close-button'); //закрытие попапа формы добавления
+const elementContainer = document.querySelector('.elements'); //грид картинок
 
-const popupPic = document.querySelector('.pic-popup');
+ //открытие-закрытие попапов
 
-function openPopup(){
-  popup.classList.add('popup_opened');
+function openPopup(pop){ //функция открытия попапов
+  pop.classList.add('popup_opened');
+  name.value = nameProfile.textContent; //в форму переносится со странички
+  about.value = aboutProfile.textContent; //в форму переносится со странички
+}
+
+editButton.addEventListener('click', openPopup.bind(editButton, popup)); //обработчик кнопки редактирования
+addButton.addEventListener('click', openPopup.bind(addButton, popupAdd)); //обработчик кнопки добавления +
+
+function closePopup(pop){ //функция закрытия попапов
+  pop.classList.remove('popup_opened');
   name.value = nameProfile.textContent;
-  about.value = aboutProfile.textContent;
-}
-function openPopupAdd(){
-  popupAdd.classList.add('add-popup_opened');
+  about.value = aboutProfile.textContent; 
 }
 
-function closePopup(){
-  popup.classList.remove('popup_opened');
-}
+closeButton.addEventListener('click', closePopup.bind(closeButton, popup)); //обработчик кнопки закрытия редактирования
+closeButtonPic.addEventListener('click', closePopup.bind(closeButtonPic, popupAdd));//обработчик кнопки закрытия формы добавления
 
-function closePopupAdd(){
-  popupAdd.classList.remove('add-popup_opened');
-}
+//добавление данных и перенос их на страницу
 
-function formSubmitHandler(evt){
+function formSubmitHandler(evt){ //для подтягивания в профиль забитых значений из формы (имя и работа)
   evt.preventDefault();
   nameProfile.textContent = name.value;
   aboutProfile.textContent = about.value;
-  closePopup();
+  closePopup(popup);
+}
+container.addEventListener('submit', formSubmitHandler); //перенос данных формы по клику Submit (имя и работа) и закрытие
+
+//создаем карточки из массива
+
+const addElement = document.querySelector('#element');//выбрали темплэйт
+
+function addElementContainer (element){ //делаем контейнер под карточку
+  const cloneCard = addElement.content.cloneNode(true); //клонируем карточку из темплэйта
+  const elImage = cloneCard.querySelector('.element__image');
+  const elTitle = cloneCard.querySelector('.element__title');
+  elImage.src = element.link; //ссылка вытаскивает картинку
+  elTitle.textContent = element.name;  //в подпись подтягивает имя из массива
+     
+    //задействуем кнопку "лайк"
+    const likeButton = cloneCard.querySelector('.element__like');// объявляем кнопку Лайк 
+      function likeBtn(evt){  
+        evt.target.classList.toggle('element__like_active');
+      } 
+    likeButton.addEventListener('click', likeBtn);    
+    
+    //задействуем кнопку "удалить"
+    const delButton = cloneCard.querySelector('.element__delete-card'); //объявляем кнопку корзина
+    const elementCard = cloneCard.querySelector('.element'); //объявляем элемент под удаление
+    function deleteElement(){  //функция удаления карточки
+      elementCard.classList.add('element_non-display'); //добавляет класс не показывать карточку (удалить)
+    }
+    delButton.addEventListener('click', deleteElement); //запускаем удаление по клику
+    
+   //задействуем функцию увеличения картинки
+
+    const popupPic = document.querySelector('.pic-popup'); //попап увеличения картинки
+    function openPopupPic(){ //функция открытия увеличенной картинки
+      openPopup(popupPic); //добавляет класс для открытия попапа картинки
+      popupPic.querySelector('.pic-popup__content').src = elImage.src;  //подтягивает картинку из объявленной
+      popupPic.querySelector('.pic-popup__name').textContent = element.name; //подтягивает имя элемента
+    }
+    elImage.addEventListener('click', openPopupPic); //задействует картинку для клика-увеличения
+
+    const closePicBtn = popupPic.querySelector('.pic-popup__close-button'); //объявили кнопку закрытия картинки
+    closePicBtn.addEventListener('click', closePopup.bind(closePicBtn, popupPic));//задействуем кнопку закрытия по клику 
+
+  elementContainer.prepend(cloneCard); //добавляем элемент в контейнер
 }
 
-
-const initialCards = [
-  {   
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
- 
-const addElementContainer = element => {
-  const addElement = document.querySelector('#element').content.cloneNode(true);
-  addElement.querySelector('.element__image').src = element.link;
-  addElement.querySelector('.element__title').textContent = element.name;  
-
-  let likeButton = addElement.querySelector('.element__like');
-  function likeBtn(evt){  
-    evt.target.classList.toggle('element__like_active');
-  }
-  likeButton.addEventListener('click', likeBtn);
-
-  let delButton = addElement.querySelector('.element__delete-card');
-  const elementCard = addElement.querySelector('.element');
-  function deleteElement(){ 
-    elementCard.classList.add('element_non-display');
-  }
-  delButton.addEventListener('click', deleteElement);
-
-  const picture = addElement.querySelector('.element__image');
-  function openPopupPic(){
-    popupPic.classList.add('pic-popup_opened');
-    popupPic.querySelector('.pic-popup__content').src = picture.src;
-    popupPic.querySelector('.pic-popup__name').textContent = element.name;
-  }
-  picture.addEventListener('click', openPopupPic);
-
-  const closePicBtn = popupPic.querySelector('.pic-popup__close-button');
-  function closePopupPic(){
-    popupPic.classList.remove('pic-popup_opened');
-  }
-  closePicBtn.addEventListener('click', closePopupPic);
-
-  elementContainer.append(addElement); 
-}
- 
-initialCards.forEach(addElementContainer);
+initialCards.forEach(addElementContainer); //по каждому элементу массива проходится функция (6 раз) 
   
-function formSubmitPic(evt){
+//добавляем новые карточки
+
+const namePic = addContainer.querySelector('#namePic');//название картинки в форме
+const linkPic = addContainer.querySelector('#link');//ссылка на картинку в форме
+function formSubmitPic(evt){ //функция добавления нового элемента
+  const newCard = {name: namePic.value, link: linkPic.value};
   evt.preventDefault();
-  const newCard = document.querySelector('#element').content.cloneNode(true);
-  newCard.querySelector('.element__image').src = linkPic.value;
-  newCard.querySelector('.element__title').textContent = namePic.value;
-  closePopupAdd();
-  
-  let likeButton = newCard.querySelector('.element__like');
-  function likeBtn(evt){  
-  evt.target.classList.toggle('element__like_active');
-  }
-  likeButton.addEventListener('click', likeBtn);
-
-  let delButton = newCard.querySelector('.element__delete-card');
-  const elementCard = newCard.querySelector('.element');
-  function deleteElement(evt){ 
-    elementCard.classList.add('element_non-display');
-  }
-  delButton.addEventListener('click', deleteElement);
-
-  const picture = newCard.querySelector('.element__image');
-  function openPopupPic(){
-    popupPic.classList.add('pic-popup_opened');
-    popupPic.querySelector('.pic-popup__content').src = picture.src;
-    popupPic.querySelector('.pic-popup__name').textContent = namePic.value;
-  }
-  picture.addEventListener('click', openPopupPic);
-
-  const closePicBtn = popupPic.querySelector('.pic-popup__close-button');
-  function closePopupPic(){  
-    popupPic.classList.remove('pic-popup_opened');
-  }
-  closePicBtn.addEventListener('click', closePopupPic);
-
-  elementContainer.prepend(newCard);
+  addElementContainer(newCard);
+  closePopup(popupAdd); //закрываем Попап добавления картинок-элементов
 }
-addContainer.addEventListener('submit', formSubmitPic);
-
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-container.addEventListener('submit', formSubmitHandler);
-
-addButton.addEventListener('click', openPopupAdd);
-closeButtonPic.addEventListener('click', closePopupAdd);
-
-
-
-
+addContainer.addEventListener('submit', formSubmitPic);// задействуем форму добавления элемента по клику
